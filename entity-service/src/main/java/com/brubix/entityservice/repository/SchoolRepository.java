5637e33be4e6fd91e2944159120488d2943a9fff
sanjeev.singh <sanjeev.singh1@olacabs.com>
2017-09-14 22:51:30 +0530

    Ticket-1 - Entity Service Domain Modeling

---
+++ entity-service/src/main/java/com/brubix/entityservice/repository/SchoolRepository.java
@@ -0,0 +1,13 @@
+package com.brubix.entityservice.repository;
+
+
+import com.brubix.model.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by sanjeev.singh1 on 14/09/17.
+ */
+public interface SchoolRepository extends JpaRepository<School, Long> {
+
+
+}
---
+++ entity-service/src/test/java/com/brubix/entityservice/repository/SchoolRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.brubix.entityservice.repository;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sanjeev.singh1 on 14/09/17.
+ */
+public class SchoolRepositoryTest {
+
+}
