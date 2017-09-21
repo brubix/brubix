d7306d2020d498f12f30bbc38b5bb912fca987ed
sanjaya <sanjaya.bigrubix@gmail.com>
2017-09-21 14:03:15 +0530

    [Sanjaya] : banner.txt / READ.ME modification

--- README.md
+++ README.md
@@ -9,3 +9,17 @@
     mysql> create user 'brubix'@'localhost' identified by 'brubix';
     mysql> grant all on brubix.* to 'brubix'@'localhost';    
 
+## Running b-Rubix in local ##
+    $ cd brubix
+    $ mvn clean install
+    $ cd b-rubix-service
+    $ mvn spring-boot:run
+
+## b-Rubix Swagger ##  
+    http://localhost:8080/v1/swagger-ui.html
+    
+## b-Rubix Sample Data ##
+    https://github.com/brubix/brubix/tree/master/b-rubix-service/src/test/java/com/brubix/brubixservice/data
+      
+  
+    
--- b-rubix-service/src/main/java/com/brubix/brubixservice/controller/inventory/school/SchoolLoaderController.java
+++ b-rubix-service/src/main/java/com/brubix/brubixservice/controller/inventory/school/SchoolLoaderController.java
@@ -10,10 +10,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
@@ -40,8 +37,7 @@ public class SchoolLoaderController {
     @ApiOperation(
             value = "Load school",
             notes = "Load school",
-            code = 200,
-            response = String.class)
+            code = 200, response = SchoolCreationResult.class)
     @ApiResponses(
             value = {
                     @ApiResponse(code = 400, message = INVALID_PAYLOAD, response = ErrorResponse.class),
@@ -49,16 +45,17 @@ public class SchoolLoaderController {
                     @ApiResponse(code = 405, message = INVALID_METHOD, response = ErrorResponse.class),
                     @ApiResponse(code = 500, message = INTERNAL_ERROR, response = ErrorResponse.class)
             })
-    public ResponseEntity<List<SchoolCreationResult>> createSchool(
+    @ResponseBody
+    public ResponseEntity<SchoolCreationResult> createSchool(
             @ApiParam(name = "School",
                     value = "School to be loaded",
-                    required = true) @Valid SchoolForm schoolForm,
+                    required = true) @Valid @RequestBody SchoolForm schoolForm,
             @ApiParam(name = "School Logo",
                     value = "School Logo",
                     required = true) @RequestParam(value = "file") MultipartFile schoolLogo) {
         schoolForm.setSchoolLogoFile(schoolLogo);
         List<SchoolCreationResult> schoolCreationResults = schoolDataLoader.load(Arrays.asList(schoolForm));
-        return new ResponseEntity<>(schoolCreationResults, HttpStatus.OK);
+        return new ResponseEntity<>(schoolCreationResults.stream().findFirst().get(), HttpStatus.OK);
     }
 
 }
