e7912d412c17f28150f4c1fc049cf7ccdda309a6
Sanjaya <sanjayakumar.sahoo@in.tesco.com>
2017-10-02 09:42:36 +0530

    [Sanjaya] : adding module b-Rubix Identity

---
+++ b-rubix-identity/src/main/java/com/brubix/identity/BrubixIdentityApplication.java
@@ -0,0 +1,13 @@
+package com.brubix.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrubixIdentityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrubixIdentityApplication.class, args);
+    }
+}
+
