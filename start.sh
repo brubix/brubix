11c3734cd80723e933690f4a372af10b6b1b779d
Sanjaya <sanjayakumar.sahoo@in.tesco.com>
2017-10-20 19:55:51 +0530

    [Sanjaya] : starting b-rubix-service container

--- start.sh
+++ start.sh
@@ -7,17 +7,11 @@
 ./build.sh
 
 #start mysql container
-docker run --name b-rubix-mysql -e MYSQL_ROOT_PASSWORD=admin -e MYSQL_DATABASE=brubix -e MYSQL_USER=brubix -e MYSQL_PASSWORD=brubix -p 3306:3306  -d mysql
+docker run --name b-rubix-mysql -e MYSQL_ROOT_PASSWORD=admin -e MYSQL_DATABASE=brubix -e MYSQL_USER=brubix -e MYSQL_PASSWORD=brubix -p 3306:3306  -d mysql:latest
 
 # to mysql to start
-echo "Waiting 30 seconds for MySQL to come up"
-sleep 30s
+echo "Waiting 20 seconds for MySQL to come up"
+sleep 20s
 
 # start b-rubix-service container
-# FIXME - link containers, to access MySql
-# docker run --name b-rubix-service -p 8080:8080 -d b-rubix-service:SNAPSHOT
-# docker run --name b-rubix-service --link b-rubix-mysql:mysql -p 8080:8080 -d b-rubix-service:SNAPSHOT
-
-# run by maven
-cd b-rubix-service
-mvn spring-boot:run
+docker run --name b-rubix-service --link b-rubix-mysql:localhost -p 8080:8080 -d b-rubix-service:SNAPSHOT
--- stop.sh
+++ stop.sh
@@ -3,17 +3,13 @@
 # stop mysql
 echo "Stopping MySQL container"
 docker stop b-rubix-mysql
-echo "MySQL container stopped"
 
 echo "Removing MySQL container"
 docker rm b-rubix-mysql
-echo "MySQL container removed "
 
 # stop b-rubix-service
 echo "Stopping b-rubix service container"
 docker stop b-rubix-service
-echo "b-rubix service container stopped"
 
 echo "Removing b-rubix service container"
 docker rm b-rubix-service
-echo "b-rubix service container removed"
