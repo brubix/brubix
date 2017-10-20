a8c18c38168fff3158dc68a9bcf80327d7f7bcb4
sanjeev.singh <sanjeev.singh1@olacabs.com>
2017-10-20 18:57:23 +0530

    Integrating School Onboarding

---
+++ b-rubix-service/src/main/resources/db/migration/V1__Init.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA IF NOT EXISTS brubix;
+
+USE brubix;
+
+INSERT INTO `country` (`id`, `code`, `currency`, `description`)
+VALUES
+  (1, 'IND', 'INR', 'India'),
+  (2, 'USA', 'USA', 'United States of Ame');
